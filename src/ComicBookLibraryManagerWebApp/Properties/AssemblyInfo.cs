1145f67525bc4d3a31607f0b361257061210040c
Lee-Michael Owen <LeeMichaelO@outlook.com>
2019-01-25 04:20:32 -0700

    Added Treehouse files

--- src/ComicBookLibraryManagerWebApp/Properties/AssemblyInfo.cs
+++ src/ComicBookLibraryManagerWebApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ComicBookLibraryManagerWebApp")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("750e8a7e-3981-4597-a41a-0b32ad01714d")]
+[assembly: Guid("b2eec0b8-a988-40f7-be7b-604b5c05d65d")]
 
 // Version information for an assembly consists of the following four values:
 //
